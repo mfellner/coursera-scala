6ffb390140caa4bcc2848111871e588cc1486256
Maximilian Fellner <max.fellner@gmail.com>
2016-05-29 02:28:29 +0200

    feat: implement pascal

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,39 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    def recur(xs: Vector[Int], k: Int): Int = {
+      if (c == k - 1)
+        xs.last
+      else
+        recur(xs :+ (xs.last * ((r + 1 - k) / k.toDouble)).toInt, k + 1)
+    }
+    recur(Vector(1), 1)
   }
+
+  def pascal2(c: Int, r: Int): Int = {
+    def recur(xs: Vector[Int]): Vector[Int] = {
+      if (r == xs.length - 1)
+        xs
+      else {
+        val ys = (for {
+          i <- xs.indices.dropRight(1)
+        } yield xs(i) + xs(i + 1)).toVector
+        recur(1 +: ys :+ 1)
+      }
+    }
+    recur(Vector(1))(c)
+  }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
