def7e541428c925cebd39a41b6f43d5130ae64c9
Maximilian Fellner <max.fellner@gmail.com>
2016-06-18 22:05:11 +0200

    feat: implement parBalance

--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -41,22 +41,58 @@ object ParallelParenthesesBalancing {
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
     */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    @tailrec
+    def recur(chars: Array[Char], open: Int): Boolean = {
+      if (open < 0)
+        false
+      else if (chars.isEmpty)
+        open == 0
+      else {
+        val x = chars.head match {
+          case '(' => 1
+          case ')' => -1
+          case _ => 0
+        }
+        recur(chars.tail, open + x)
+      }
+    }
+    recur(chars, 0)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
     */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
-
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    @tailrec
+    def traverse(idx: Int, until: Int, open: Int, close: Int): (Int, Int) = {
+      if (idx == until)
+        (open, close)
+      else {
+        val diff = math.abs(open) - math.abs(close)
+        val open2 = chars(idx) match {
+          case '(' => open + 1
+          case ')' => open - 1
+          case _ => open
+        }
+        val close2 = chars(idx) match {
+          case ')' => if (open2 < 0) close - 1 else close
+          case _ => close
+        }
+        traverse(idx + 1, until, open2, close2)
+      }
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from < threshold || threshold < 2) {
+        val (open, close) = traverse(from, until, 0, 0)
+        (open, close)
+      } else {
+        val mid = (from + until) / 2
+        val ((open1, close1), (open2, close2)) = parallel(reduce(from, mid), reduce(mid, until))
+        (open1 + open2, open1 + close2)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) ==(0, 0)
   }
 
   // For those who want more:
--- reductions/src/test/scala/reductions/ParallelParenthesesBalancingSuite.scala
+++ reductions/src/test/scala/reductions/ParallelParenthesesBalancingSuite.scala
@@ -45,5 +45,94 @@ class ParallelParenthesesBalancingSuite extends FunSuite {
     check(").", false)
   }
 
+  // Parallel implementation
 
+  test("parBalance should work for empty string") {
+    def check(input: String, expected: Boolean) =
+      assert(parBalance(input.toArray, 4) == expected,
+        s"parBalance($input) should be $expected")
+
+    check("", true)
+  }
+
+  test("parBalance should work for string of length 1") {
+    def check(input: String, expected: Boolean) =
+      assert(parBalance(input.toArray, 4) == expected,
+        s"parBalance($input) should be $expected")
+
+    check("(", false)
+    check(")", false)
+    check(".", true)
+  }
+
+  test("parBalance should work for strings of different lengths with threshold 2") {
+    def check(input: String, threshold: Int, expected: Boolean) =
+      assert(parBalance(input.toArray, threshold) == expected,
+        s"parBalance($input) should be $expected")
+
+    check(".)", 2, false)
+    check(".(", 2, false)
+    check("(.", 2, false)
+    check(").", 2, false)
+    check(")", 2, false)
+    check("(", 2, false)
+    check("((", 2, false)
+    check("))", 2, false)
+    check(")(", 2, false)
+    check("(((", 2, false)
+    check(")))", 2, false)
+    check("))))", 2, false)
+    check("((((", 2, false)
+    check(")()(", 2, false)
+    check(")()()()(", 2, false)
+    check(")(())(", 2, false)
+    check(")((", 2, false)
+    check("))(", 2, false)
+    check("()(", 2, false)
+    check(")()", 2, false)
+
+    check("()", 2, true)
+    check("()()", 2, true)
+    check("()()()", 2, true)
+    check("()()()()", 2, true)
+    check("(())", 2, true)
+    check("((()))", 2, true)
+    check("(((())))", 2, true)
+    check("(())()", 2, true)
+    check("()(())", 2, true)
+    check("()(())()(())", 2, true)
+  }
+
+  test("parBalance should work for strings of different lengths with threshold 3") {
+    def check(input: String, threshold: Int, expected: Boolean) =
+      assert(parBalance(input.toArray, threshold) == expected,
+        s"parBalance($input) should be $expected")
+
+    check("((", 3, false)
+    check("))", 3, false)
+    check(")(", 3, false)
+    check("(((", 3, false)
+    check(")))", 3, false)
+    check("))))", 3, false)
+    check("((((", 3, false)
+    check(")()(", 3, false)
+    check(")()()()(", 3, false)
+    check(")(())(", 3, false)
+    check(")((", 3, false)
+    check("))(", 3, false)
+    check("()(", 3, false)
+    check(")()", 3, false)
+
+    check("()", 3, true)
+    check("()()", 3, true)
+    check("()()()", 3, true)
+    check("()()()()", 3, true)
+    check("(())", 3, true)
+    check("((()))", 3, true)
+    check("(((())))", 3, true)
+    check("(())()", 3, true)
+    check("()(())", 3, true)
+    check("()(())()(())", 3, true)
+    check("()((foo))(bar)((.))", 3, true)
+  }
 }
