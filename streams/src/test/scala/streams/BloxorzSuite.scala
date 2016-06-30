d30b3eeab9e0ccfca5b3075bc061d6a07e144d37
Maximilian Fellner <maximilian.fellner@zalando.de>
2016-06-30 11:00:01 +0200

    feat: implement solver

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -76,4 +76,30 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighborsWithHistory") {
+    new Level1 {
+      val result = neighborsWithHistory(Block(Pos(1, 1), Pos(1, 1)), List(Left, Up)).toSet
+      val expected = Set(
+        (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)),
+        (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up)))
+
+      assert(result == expected)
+    }
+  }
+
+  test("newNeighborsOnly") {
+    new Level1 {
+      val neighbors = Set(
+        (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)),
+        (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up)))
+
+      val explored = Set(Block(Pos(1, 2), Pos(1, 3)), Block(Pos(1, 1), Pos(1, 1)))
+
+      val result = newNeighborsOnly(neighbors.toStream, explored).toSet
+      val expected = Set((Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up)))
+
+      assert(result == expected)
+    }
+  }
+
 }
