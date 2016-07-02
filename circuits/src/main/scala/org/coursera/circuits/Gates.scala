2db8c07fc68b65020abd73b17c274295f84abe53
Maximilian Fellner <max.fellner@gmail.com>
2016-07-02 15:29:09 +0200

    feat: add circuits

---
+++ circuits/src/main/scala/org/coursera/circuits/Gates.scala
@@ -0,0 +1,86 @@
+package org.coursera.circuits
+
+trait Gates extends Simulation {
+  def InverterDelay: Int
+
+  def AndGateDelay: Int
+
+  def OrGateDelay: Int
+
+  class Wire {
+    private var sigVal = false
+    private var actions: List[Action] = List()
+
+    def getSignal: Boolean = sigVal
+
+    def setSignal(s: Boolean): Unit = {
+      if (s != sigVal) {
+        sigVal = s
+        actions foreach (_ ())
+      }
+    }
+
+    def addAction(a: Action): Unit = {
+      actions = a :: actions
+      a()
+    }
+  }
+
+  def inverter(input: Wire, output: Wire): Unit = {
+    def invertAction(): Unit = {
+      val inputSig = input.getSignal
+      afterDelay(InverterDelay) {
+        output setSignal !inputSig
+      }
+      input addAction invertAction
+    }
+  }
+
+  def andGate(in1: Wire, in2: Wire, output: Wire): Unit = {
+    def andAction(): Unit = {
+      val in1Sig = in1.getSignal
+      val in2Sig = in2.getSignal
+      afterDelay(AndGateDelay) {
+        output setSignal (in1Sig & in2Sig)
+      }
+    }
+    in1 addAction andAction
+    in2 addAction andAction
+  }
+
+  def orGate(in1: Wire, in2: Wire, output: Wire): Unit = {
+    def orAction(): Unit = {
+      val in1Sig = in1.getSignal
+      val in2Sig = in2.getSignal
+      afterDelay(OrGateDelay) {
+        output setSignal (in1Sig | in2Sig)
+      }
+    }
+    in1 addAction orAction
+    in2 addAction orAction
+  }
+
+  def orGateAlt(in1: Wire, in2: Wire, output: Wire): Unit = {
+    val notIn1, notIn2, notOut = new Wire
+    inverter(in1, notIn1)
+    inverter(in2, notIn2)
+    andGate(notIn1, notIn2, notOut)
+    inverter(notOut, output)
+  }
+
+  def xorGate(a: Wire, b: Wire, c: Wire): Unit = {
+    val d, e, f, g = new Wire
+    inverter(a, d)
+    inverter(b, e)
+    andGate(a, e, f)
+    andGate(b, d, g)
+    orGate(f, g, c)
+  }
+
+  def probe(name: String, wire: Wire): Unit = {
+    def probeAction(): Unit = {
+      println(s"$name $currentTime new value = ${wire.getSignal}")
+    }
+    wire addAction probeAction
+  }
+}
